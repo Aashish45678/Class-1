eb6ca55a929790d6f2197513d68f029aceb8b3a4
Ashish Sapkota <107096495+Aashish45678@users.noreply.github.com>
2022-11-29 11:07:37 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+//This is comment
+#This is comment
+/*..........Enter multiple
+line comment..................*/
+echo "<h1>hello world</h1><br>";
+$x= 5;
+$y= 6;
+echo $x+$y,"<br>";
+var_dump($x);
+echo "<br>";
+
+$x+=$y;
+echo $x;
+?>
