59845e629cd0114f2b251ad33facd9eb5b88170a
Ashish Sapkota <107096495+Aashish45678@users.noreply.github.com>
2022-12-06 09:24:19 +0545

    Add files via upload

--- index.php
+++ index.php
@@ -12,4 +12,35 @@ echo "<br>";
 
 $x+=$y;
 echo $x;
+
+if ($x<12){
+echo "Good morning,<br>";
+}
+else{
+echo "Good afternoon";
+}
+
+#first question to print even number upto 100 except 44
+$i=0;
+for ($i=0;$i<=100;$i=$i+2)
+{
+if ($i==44){
+    continue;
+}
+else if ($i==100){
+    echo $i."<br>";
+}
+else{
+    echo $i.",";
+}
+}
+
+#to print multiplication table of 45
+$j=45;
+$n=1;
+for ( $n = 1 ; $n <= 10 ; $n++ )
+{
+    $m=$j*$n;
+    echo $j."*".$n."=".$m."<br>";
+}
 ?>
